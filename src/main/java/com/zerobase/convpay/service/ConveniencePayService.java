cb44a267d45928b6277417a39172db9f15d15510
YeoniSon <sjy8211@gmail.com>
2025-06-25 18:23:47 +0900

    project -3 UserClient까지 완성

--- src/main/java/com/zerobase/convpay/service/ConveniencePayService.java
+++ src/main/java/com/zerobase/convpay/service/ConveniencePayService.java
@@ -1,7 +1,9 @@
 package com.zerobase.convpay.service;
 
 import com.zerobase.convpay.dto.*;
+import com.zerobase.convpay.type.MoneyUseCancelResult;
 import com.zerobase.convpay.type.MoneyUseResult;
+import com.zerobase.convpay.type.PayCancelResult;
 import com.zerobase.convpay.type.PayResult;
 
 public class ConveniencePayService { //편결이
@@ -32,7 +34,12 @@ public class ConveniencePayService { //편결이
         return new PayResponse(PayResult.SUCCESS, payRequest.getPayAmount());
     }
 
-    public PayCancelResponse payCancel(PayCancelRequest payCancelRequest) {
-        moneyAdapter.useCancel(payCancelRequest.getPayCancelAmount());
+    public static PayCancelResponse payCancel(PayCancelRequest payCancelRequest) {
+        MoneyUseCancelResult moneyUseCancelResult = moneyAdapter.useCancel(payCancelRequest.getPayCancelAmount());
+
+        if (moneyUseCancelResult == MoneyUseCancelResult.MONEY_USE_CANCEL_FAIL) {
+            return new PayCancelResponse(PayCancelResult.PAY_CANCEL_FAIL, 0);
+        }
+        return new PayCancelResponse(PayCancelResult.PAY_CANCEL_SUCCESS, payCancelRequest.getPayCancelAmount());
     }
 }
--- src/test/java/com/zerobase/convpay/service/ConveniencePayServiceTest.java
+++ src/test/java/com/zerobase/convpay/service/ConveniencePayServiceTest.java
@@ -1,21 +1,25 @@
 package com.zerobase.convpay.service;
 
+import com.zerobase.convpay.dto.PayCancelRequest;
+import com.zerobase.convpay.dto.PayCancelResponse;
 import com.zerobase.convpay.type.ConvenienceType;
 import com.zerobase.convpay.dto.PayRequest;
 import com.zerobase.convpay.dto.PayResponse;
+import com.zerobase.convpay.type.PayCancelResult;
 import com.zerobase.convpay.type.PayResult;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConveniencePayServiceTest {
+    ConveniencePayService conveniencePayService = new ConveniencePayService();
 
     @Test
     void pay_success() {
         //given
         PayRequest payRequest = new PayRequest(ConvenienceType.G25, 50);
         //when
-        PayResponse payResponse = ConveniencePayService.pay(payRequest);
+        PayResponse payResponse = conveniencePayService.pay(payRequest);
         //then
         assertEquals(PayResult.SUCCESS, payResponse.getPayResult());
         assertEquals(50, payResponse.getPaidAmount());
@@ -26,10 +30,32 @@ class ConveniencePayServiceTest {
         //given
         PayRequest payRequest = new PayRequest(ConvenienceType.G25, 1000_001);
         //when
-        PayResponse payResponse = ConveniencePayService.pay(payRequest);
+        PayResponse payResponse = conveniencePayService.pay(payRequest);
         //then
         assertEquals(PayResult.FAIL, payResponse.getPayResult());
         assertEquals(0, payResponse.getPaidAmount());
+    }
+
+    @Test
+    void pay_cancel_success() {
+        //given
+        PayCancelRequest payCancelRequest = new PayCancelRequest(ConvenienceType.G25, 1000);
+        //when
+        PayCancelResponse payCancelResponse = conveniencePayService.payCancel(payCancelRequest);
+        //then
+        assertEquals(PayCancelResult.PAY_CANCEL_SUCCESS, payCancelResponse.getPayCancelResult());
+        assertEquals(1000, payCancelResponse.getPayCanceledAmount());
+    }
+
+    @Test
+    void pay_cancel_fail() {
+        //given
+        PayCancelRequest payRequest = new PayCancelRequest(ConvenienceType.G25, 99);
+        //when
+        PayCancelResponse payResponse = conveniencePayService.payCancel(payRequest);
+        //then
+        assertEquals(PayCancelResult.PAY_CANCEL_FAIL, payResponse.getPayCancelResult());
+        assertEquals(0, payResponse.getPayCanceledAmount());
     }
 
 }
