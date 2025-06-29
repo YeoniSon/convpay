1112e45355ca6174c8d481b0e49ddecef7d38b72
YeoniSon <sjy8211@gmail.com>
2025-06-29 17:28:20 +0900

    Spring Resource(외부 자원 가져오기)

--- src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -2,10 +2,15 @@ package com.zerobase.convpay.config;
 
 import com.zerobase.convpay.ConvpayApplication;
 import com.zerobase.convpay.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -37,4 +42,13 @@ public class ApplicationConfig {
 //    public DiscountByConvenience discountByConvenience() {
 //        return new DiscountByConvenience();
 //    }
+    @Autowired
+    //resource 적용함
+    private ApplicationContext applicationContext;
+
+    public void getResource() throws IOException {
+        Resource resource = applicationContext.getResource("myTemplate.txt");
+
+        System.out.println(resource.contentLength() + "");
+    }
 }
